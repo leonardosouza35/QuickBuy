0c1b42bb5d45168afd90f50161f3317ca6b78d57
leonardosouza35 <leonardosouza35@outlook.com>
2019-04-14 22:32:10 -0300

    repositorio

--- QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
@@ -1,45 +1,52 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using QuickBuy.Dominio.Contratos;
+using QuickBuy.Repositorio.Contexto;
 
 namespace QuickBuy.Repositorio.Repositorios
 {
     public class BaseRepositorio<TEntity> : IBaseRepositorio<TEntity> where TEntity : class
     {
 
+        protected readonly QuickBuyContexto QuickBuyContexto;
 
-        public BaseRepositorio()
+               
+        public BaseRepositorio(QuickBuyContexto quickBuyContexto)
         {
-
+            QuickBuyContexto = quickBuyContexto;
         }
 
         public void Adicionar(TEntity entity)
         {
-            throw new System.NotImplementedException();
+            QuickBuyContexto.Set<TEntity>().Add(entity);
+            QuickBuyContexto.SaveChanges();
         }
 
         public void Atualizar(TEntity entity)
         {
-            throw new System.NotImplementedException();
+            QuickBuyContexto.Set<TEntity>().Update(entity);
+            QuickBuyContexto.SaveChanges();
         }
         
         public TEntity ObterPorId(int id)
         {
-            throw new System.NotImplementedException();
+            return QuickBuyContexto.Set<TEntity>().Find(id);
         }
 
         public IEnumerable<TEntity> ObterTodos()
         {
-            throw new System.NotImplementedException();
+            return QuickBuyContexto.Set<TEntity>().ToList();
         }
 
         public void Remover(TEntity entity)
         {
-            throw new System.NotImplementedException();
+            QuickBuyContexto.Remove(entity);
+            QuickBuyContexto.SaveChanges();
         }
 
         public void Dispose()
         {
-            throw new System.NotImplementedException();
+            QuickBuyContexto.Dispose();
         }
     }
 }
--- QuickBuy.Repositorio/Repositorios/ProdutoRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/ProdutoRepositorio.cs
@@ -1,10 +1,14 @@
 ﻿using QuickBuy.Dominio.Contratos;
 using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Contexto;
 
 namespace QuickBuy.Repositorio.Repositorios
 {
-    public class ProdutoRepositorio: BaseRepositorio<Produto>, IProdutoRepositorio
+    public class ProdutoRepositorio : BaseRepositorio<Produto>, IProdutoRepositorio
     {
+        public ProdutoRepositorio(QuickBuyContexto quickBuyContexto) : base(quickBuyContexto)
+        {
 
+        }
     }
 }
