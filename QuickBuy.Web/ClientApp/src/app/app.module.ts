5dbfbdc55e375289ca304ea6bf8b2fb14c27e4d9
leonardosouza35 <leonardosouza35@outlook.com>
2019-06-25 18:18:18 -0300

    alterações da seção de template

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ProdutoComponent } from './produto/produto.component'
+import { LoginComponent } from './usuario/login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ProdutoComponent } from './produto/produto.component'
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    ProdutoComponent
+    ProdutoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,7 +30,8 @@ import { ProdutoComponent } from './produto/produto.component'
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'produto', component: ProdutoComponent }
+      { path: 'produto', component: ProdutoComponent },
+      { path: 'entrar', component: LoginComponent }
     ])
   ],
   providers: [],
