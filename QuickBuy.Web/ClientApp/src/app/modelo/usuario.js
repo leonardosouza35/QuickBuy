1705e3b99c2175ee3415c11f22a34442d519d895
leonardosouza35 <leonardosouza35@outlook.com>
2019-06-26 14:05:39 -0300

    alterações aulas de template Angular

---
+++ QuickBuy.Web/ClientApp/src/app/modelo/usuario.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Usuario = /** @class */ (function () {
+    function Usuario() {
+    }
+    return Usuario;
+}());
+exports.Usuario = Usuario;
+//# sourceMappingURL=usuario.js.map
---
+++ QuickBuy.Web/ClientApp/src/app/modelo/usuario.ts
@@ -0,0 +1,7 @@
+export class Usuario {
+  id: number;
+  email: string;
+  senha: string;
+  nome: string;
+  sobreNome: string;
+}
