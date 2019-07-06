e4abe32c02a692ac652ee7da82c4d3e57960655d
leonardosouza35 <leonardosouza35@outlook.com>
2019-07-06 19:41:15 -0300

    validação fase 1

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -8,17 +8,13 @@ import { Usuario } from "../../modelo/usuario";
 })
 export class LoginComponent {
   public usuario;  
-  public usuarioAutenticado: boolean;
-
-  public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"];
-
+    
   constructor() {
     this.usuario = new Usuario();
   }
   
   entrar() {
-    if (this.usuario.email == "leo@teste.com" && this.usuario.senha == "abc123") {
-      this.usuarioAutenticado = true;
+    if (this.usuario.email == "leo@teste.com" && this.usuario.senha == "abc123") {      
     }
   }
     
