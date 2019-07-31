ef23d9082900ac07ce155d65da7e0b337effc3d1
leonardosouza35 <leonardosouza35@outlook.com>
2019-07-31 14:09:48 -0300

    template produto

--- QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core"
 
 @Component({
   selector: "app-produto",
-  template : "<html><body>{{ obterNome() }}</body></html>"
+  templateUrl: "./produto.component.html",
+  styleUrls: ["./produto.component.css"]
 })
 export class ProdutoComponent {// Nome das classes começando com maíusculo por conta da convenção PascalCase
 
