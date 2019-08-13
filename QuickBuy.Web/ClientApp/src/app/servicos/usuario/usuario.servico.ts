0825543f4f4f70bc37f5d46bc7901722ed75ef3c
leonardosouza35 <leonardosouza35@outlook.com>
2019-08-12 22:30:33 -0300

    usuario servico

--- QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
+++ QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
@@ -32,7 +32,10 @@ export class UsuarioServico {
     this._usuario = null;
   }
 
- 
+  get headers(): HttpHeaders {
+    return new HttpHeaders().set('content-type', 'application/json');
+  }
+
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.baseURL = baseUrl;
   }
@@ -51,18 +54,7 @@ export class UsuarioServico {
   }
 
   public cadastrarUsuario(usuario: Usuario): Observable<Usuario> {
-
-    const headers = new HttpHeaders().set('content-type', 'application/json');
-
-    var body = {
-      email: usuario.email,
-      senha: usuario.senha,
-      nome: usuario.nome,
-      sobreNome: usuario.sobreNome      
-    }
-
-    return this.http.post<Usuario>(this.baseURL + "api/usuario", body, { headers });
-
+    return this.http.post<Usuario>(this.baseURL + "api/usuario", JSON.stringify(usuario), { headers: this.headers });
   }
 }
 
