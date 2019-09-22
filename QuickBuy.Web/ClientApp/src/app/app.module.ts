d9d2fe92b013d65131320fb4e1d43ac55bc746a5
leonardosouza35 <leonardosouza35@outlook.com>
2019-09-22 10:22:38 -0300

    component da loja virtual

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UsuarioServico } from './servicos/usuario/usuario.servico';
 import { CadastroUsuarioComponent } from './usuario/cadastro/cadastro.usuario.component';
 import { ProdutoServico } from './servicos/produto/produto.servico';
 import { PesquisaProdutoComponent } from './produto/pesquisa/pesquisa.produto.component';
+import { LojaPesquisaComponent } from './loja/pesquisa/loja.pesquisa.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { PesquisaProdutoComponent } from './produto/pesquisa/pesquisa.produto.co
     ProdutoComponent,
     LoginComponent,
     CadastroUsuarioComponent,
-    PesquisaProdutoComponent
+    PesquisaProdutoComponent,
+    LojaPesquisaComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
---
+++ QuickBuy.Web/ClientApp/src/app/loja/pesquisa/loja.pesquisa.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core"
+import { ProdutoServico } from "../../servicos/produto/produto.servico";
+import { Produto } from "../../modelo/produto";
+
+@Component({
+  selector: "app-loja",
+  templateUrl: "./loja.pesquisa.component.html",
+  styleUrls: ["./loja.pesquisa.component.css"]
+})
+export class LojaPesquisaComponent implements OnInit {
+    public produtos: Produto[];
+
+    ngOnInit(): void {
+        
+  }
+
+  constructor(private produtoServico: ProdutoServico) {
+
+  }
+
+}
+
