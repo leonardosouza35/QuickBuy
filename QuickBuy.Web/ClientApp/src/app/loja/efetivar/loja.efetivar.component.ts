c52bbc3a566135fde4bb33547164ccffba33a8b2
leonardosouza35 <leonardosouza35@outlook.com>
2019-10-02 20:49:09 -0300

    atualizando total

--- QuickBuy.Web/ClientApp/src/app/loja/efetivar/loja.efetivar.component.ts
+++ QuickBuy.Web/ClientApp/src/app/loja/efetivar/loja.efetivar.component.ts
@@ -9,10 +9,12 @@ import { Produto } from "../../modelo/produto";
 export class LojaEfetivarComponent implements OnInit {
     public carrinhoCompras: LojaCarrinhoCompras;
     public produtos: Produto[];
+    public total: number;
 
     ngOnInit(): void {
         this.carrinhoCompras = new LojaCarrinhoCompras();
         this.produtos = this.carrinhoCompras.obterProdutos();
+        this.atualizarTotal();
     }
 
     public atualizarPreco(produto: Produto, quantidade: number) {
@@ -28,9 +30,14 @@ export class LojaEfetivarComponent implements OnInit {
         produto.preco = produto.precoOriginal * quantidade;
 
         this.carrinhoCompras.atualizar(this.produtos);
+        this.atualizarTotal();
     }
     public remover(produto: Produto) {
         this.carrinhoCompras.removerProduto(produto);
         this.produtos = this.carrinhoCompras.obterProdutos();
+        this.atualizarTotal();
+    }
+    public atualizarTotal() {
+        this.total = this.produtos.reduce((acc, produto) => acc + produto.preco, 0);
     }
 }
