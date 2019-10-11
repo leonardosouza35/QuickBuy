f7e04f1c0872e756b94ecb12b9987aa7bf17dee1
leonardosouza35 <leonardosouza35@outlook.com>
2019-10-10 23:20:51 -0300

    reativando guarda rotas

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { TruncateModule} from 'ng2-truncate';
+import { TruncateModule } from 'ng2-truncate';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -20,37 +20,36 @@ import { LojaProdutoComponent } from './loja/produto/loja.produto.component';
 import { LojaEfetivarComponent } from './loja/efetivar/loja.efetivar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,    
-    ProdutoComponent,
-    LoginComponent,
-    CadastroUsuarioComponent,
-    PesquisaProdutoComponent,
-    LojaPesquisaComponent,
-    LojaProdutoComponent,
-    LojaEfetivarComponent
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        ProdutoComponent,
+        LoginComponent,
+        CadastroUsuarioComponent,
+        PesquisaProdutoComponent,
+        LojaPesquisaComponent,
+        LojaProdutoComponent,
+        LojaEfetivarComponent
 
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    TruncateModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },      
-      { path: 'produto', component: ProdutoComponent },
-      { path: 'entrar', component: LoginComponent },
-      { path: "novo-usuario", component: CadastroUsuarioComponent },
-      { path: "pesquisar-produto", component: PesquisaProdutoComponent },
-      { path: "loja-produto", component: LojaProdutoComponent },
-      { path: "loja-efetivar", component: LojaEfetivarComponent }
-    ])
-  ],
-  providers: [UsuarioServico, ProdutoServico],
-  bootstrap: [AppComponent]
+    ],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+        HttpClientModule,
+        FormsModule,
+        TruncateModule,
+        RouterModule.forRoot([
+            { path: '', component: HomeComponent, pathMatch: 'full' },
+            { path: 'produto', component: ProdutoComponent, canActivate: [GuardaRotas] },
+            { path: 'entrar', component: LoginComponent },
+            { path: "novo-usuario", component: CadastroUsuarioComponent },
+            { path: "pesquisar-produto", component: PesquisaProdutoComponent },
+            { path: "loja-produto", component: LojaProdutoComponent },
+            { path: "loja-efetivar", component: LojaEfetivarComponent, canActivate: [GuardaRotas] }
+        ])
+    ],
+    providers: [UsuarioServico, ProdutoServico],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
 
-//{ path: 'produto', component: ProdutoComponent, canActivate: [GuardaRotas] }
